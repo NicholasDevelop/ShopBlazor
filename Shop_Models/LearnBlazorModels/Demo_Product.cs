907938ddd56d6add2e1a8fb7529fe1230b0ea421
NicholasDevelop <marcaccini.n@gmail.com>
2022-12-14 15:29:38 +0100

    Aggiunta libreria di classi ed effettuato binding al progetto server.

---
+++ Shop_Models/LearnBlazorModels/Demo_Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Models.LearnBlazorModels
+{
+    public class Demo_Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+
+        public IEnumerable<Demo_ProductProp> ProductProperties { get; set; }
+    }
+}
