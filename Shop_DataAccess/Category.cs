7a0e6202d332d7f48e4a55bd1c8915c25a3a08f4
NicholasDevelop <marcaccini.n@gmail.com>
2022-12-19 12:08:56 +0100

    Creata connessione con il DB

---
+++ Shop_DataAccess/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_DataAccess
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
