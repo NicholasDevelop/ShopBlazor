55b05051513c7528c60ee499df72b21c5a595485
NicholasDevelop <marcaccini.n@gmail.com>
2022-12-20 15:06:56 +0100

    Aggiunta validation nel form

--- Shop_Models/CategoryDTO.cs
+++ Shop_Models/CategoryDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace Shop_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shop_Models
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter name...")]
         public string Name { get; set; }
     }
 }
