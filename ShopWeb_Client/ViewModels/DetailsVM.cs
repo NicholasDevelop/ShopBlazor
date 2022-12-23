d1198928cabbee741c105c648b7b94a52736ee1c
NicholasDevelop <marcaccini.n@gmail.com>
2022-12-23 15:46:58 +0100

    Creata Classe per la gestione della selezione della Size del prodotto e sulla sua quantità all'interno della pagina di dettaglio

---
+++ ShopWeb_Client/ViewModels/DetailsVM.cs
@@ -0,0 +1,21 @@
+﻿using Shop_Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopWeb_Client.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            ProductPrice = new();
+            Count = 1;
+        }
+
+
+        [Range(1, int.MaxValue, ErrorMessage = "Please enter a value greater than 0")]
+        public int Count { get; set; }
+        [Required]
+        public int SelectedProductPriceId { get; set; }
+        public ProductPriceDTO ProductPrice { get; set; }
+    }
+}
