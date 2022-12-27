ced6e57f2ede6c6664c389d90eb6cbf39369b935
NicholasDevelop <marcaccini.n@gmail.com>
2022-12-27 16:04:02 +0100

    Creato modello ShoppingCart + CartService e la sua interfaccia ancora da sviluppare

---
+++ ShopWeb_Client/Service/CartService.cs
@@ -0,0 +1,18 @@
+﻿using ShopWeb_Client.Service.IService;
+using ShopWeb_Client.ViewModels;
+
+namespace ShopWeb_Client.Service
+{
+    public class CartService : ICartService
+    {
+        public Task DecrementCart(ShoppingCart shoppingCart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task IncrementCart(ShoppingCart shoppingCart)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ShopWeb_Client/Service/IService/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using ShopWeb_Client.ViewModels;
+
+namespace ShopWeb_Client.Service.IService
+{
+    public interface ICartService
+    {
+        Task DecrementCart(ShoppingCart shoppingCart);
+        Task IncrementCart(ShoppingCart shoppingCart);
+
+    }
+}
